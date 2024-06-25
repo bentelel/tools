da48dc13ea325c50bf78004c011aa483ee563b9e
AzureAD\LukasBentele <lukas.bentele@drjve.de>
2024-06-25 14:24:08 +0200

    made parsing engine choseable by user > in hopes that python parsing engine can deal with line-break in quotes at line end

--- CharacterCheckGUI.py
+++ CharacterCheckGUI.py
@@ -40,6 +40,9 @@ except ImportError:
 # list with available encodings for pandas.read_csv().
 available_encodings = ['ascii','big5','big5hkscs','cp037','cp273','cp424','cp437','cp500','cp720','cp737','cp775','cp850','cp852','cp855','cp856','cp857','cp858','cp860','cp861','cp862','cp863','cp864','cp865','cp866','cp869','cp874','cp875','cp932','cp949','cp950','cp1006','cp1026','cp1125','cp1140','cp1250','cp1251','cp1252','cp1253','cp1254','cp1255','cp1256','cp1257','cp1258','euc_jp','euc_jis_2004','euc_jisx0213','euc_kr','gb2312','gbk','gb18030','hz','iso2022_jp','iso2022_jp_1','iso2022_jp_2','iso2022_jp_2004','iso2022_jp_3','iso2022_jp_ext','iso2022_kr','latin_1','iso8859_2','iso8859_3','iso8859_4','iso8859_5','iso8859_6','iso8859_7','iso8859_8','iso8859_9','iso8859_10','iso8859_11','iso8859_13','iso8859_14','iso8859_15','iso8859_16','johab','koi8_r','koi8_t','koi8_u','kz1048','mac_cyrillic','mac_greek','mac_iceland','mac_latin2','mac_roman','mac_turkish','ptcp154','shift_jis','shift_jis_2004','shift_jisx0213','utf_32','utf_32_be','utf_32_le','utf_16','utf_16_be','utf_16_le','utf_7','utf_8','utf_8_sig']
 
+parsing_engines = ['c', 'python']
+DEFAULT_PARSING_ENGINE = 'c'
+
 # we are doing lookups later on using regex (within pd.df.replace) and need to escape these chars if the user wants
 # to search for them.
 characters_to_escape_in_regex = ['.', '+', '*', '?', '^', '$', '(', ')', '[', ']', '{', '}', '|', '\\']
@@ -68,6 +71,7 @@ class FileHandler:
         self.file_header = DEFAULT_FILE_HEADER_NR # zero based row index!
         self.transformed_df = pd.DataFrame([])
         self.supress_unnamed_columns = True
+        self.parsing_engine = 'c'
 
     def __new__(cls, file_path, encoding):
         if cls._instance is None:
@@ -98,15 +102,28 @@ class FileHandler:
         """Grabs dataframe from csv file and sets total length of the df within the fileHandler class."""
         # for some reason this does not work for .pyw files any longer..
         chunksize = 50000
-        if self.supress_unnamed_columns:
-            # usecols=lambda c: not c.startswith('Unnamed:') we use this to surpress unnamed cols in broken csvs
-            chunks_iter = pd.read_csv(self.path, encoding=self.encoding, low_memory=False, header=self.file_header,
-                                             dtype=str, na_values='',
-                                             usecols=lambda c: not c.startswith('Unnamed:'), engine='c', chunksize=chunksize)
+        if self.parsing_engine == 'python': # in conditional because low_memory is not supported by python engine
+            if self.supress_unnamed_columns:
+                # usecols=lambda c: not c.startswith('Unnamed:') we use this to surpress unnamed cols in broken csvs
+                chunks_iter = pd.read_csv(self.path, encoding=self.encoding, header=self.file_header,
+                                                 dtype=str, na_values='',
+                                                 usecols=lambda c: not c.startswith('Unnamed:'), engine='python', chunksize=chunksize
+                                          )
+            else:
+                chunks_iter = pd.read_csv(self.path, encoding=self.encoding, low_memory=False, header=self.file_header
+                                             , dtype=str, na_values='', engine='python', chunksize=chunksize)
+        elif self.parsing_engine == 'c':
+            if self.supress_unnamed_columns:
+                # usecols=lambda c: not c.startswith('Unnamed:') we use this to surpress unnamed cols in broken csvs
+                chunks_iter = pd.read_csv(self.path, encoding=self.encoding, low_memory=False, header=self.file_header,
+                                                 dtype=str, na_values='',
+                                                 usecols=lambda c: not c.startswith('Unnamed:'), engine='c', chunksize=chunksize
+                                          )
+            else:
+                chunks_iter = pd.read_csv(self.path, encoding=self.encoding, low_memory=False, header=self.file_header
+                                             , dtype=str, na_values='', engine='c', chunksize=chunksize)
         else:
-            chunks_iter = pd.read_csv(self.path, encoding=self.encoding, low_memory=False, header=self.file_header
-                                         , dtype=str, na_values='', engine='c', chunksize=chunksize)
-
+            raise ValueError(f"Unsupported engine {self.parsing_engine}")
         chunks = []
         for chunk in chunks_iter:
             chunks.append(chunk)
@@ -379,6 +396,9 @@ if __name__ in ("__main__", "__mp_main__"):
             with ui.row():
                 encoding_menu = ui.select(available_encodings, label='File encoding', with_input=True, value=DEFAULT_ENCODING)
                 encoding_menu.bind_value(fileHandler, 'encoding')
+                parsing_engine_menu = ui.select(parsing_engines, label='csv Parsing engine', with_input=True, value=DEFAULT_PARSING_ENGINE)
+                parsing_engine_menu.tooltip("Use c for big csv, use python for anything else.")
+                parsing_engine_menu.bind_value(fileHandler, 'parsing_engine')
                 with ui.row():
                     file_has_headers = ui.checkbox("File has headers", value=True)
                     file_has_headers.on_value_change(lambda e: fileHandler.toggle_header_mode(e.value))
